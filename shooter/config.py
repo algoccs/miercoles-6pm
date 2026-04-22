041704a90697b052373b1e77cc1298e1340b9361
algoccs <algodemoacc@gmail.com>
2026-04-22 18:43:13 -0400

    Create config.py

---
+++ shooter/config.py
@@ -0,0 +1,14 @@
+# CONSTANTES
+FPS = 60
+WIDTH, HEIGHT = 640, 480
+TITLE = 'Project Shooter'
+BACK_COL = (45, 78, 105)
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+PLAYER_IMG = 'src/ship.png'
+ENEMY_IMG = './src/enemy.png'
+BACK_IMG = './src/space.jpg'
+BGM = ''
+FONT_FILE = './src/font1.ttf'
+GAMEOVER_IMG = './src/gameover.png'
+VICTORY_IMG = ''
